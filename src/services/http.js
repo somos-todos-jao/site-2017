97366e411cb6846a3bf8543b205d4c1ccc619e50
Luigui Delyer <luigui.delyer@webradar.com>
2017-02-20 15:45:18 -0300

    troca de endpoint

--- src/services/http.js
+++ src/services/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const requester = axios.create({
-    // baseURL: 'http://www.somostodosjao.com.br/api',
-    baseURL: 'http://localhost/tekoa/back-end/tekoa-php-slim/src/public',
+    baseURL: 'http://www.somostodosjao.com.br/api',
+    // baseURL: 'http://localhost/tekoa/back-end/tekoa-php-slim/src/public',
     timeout: 1000
 })
